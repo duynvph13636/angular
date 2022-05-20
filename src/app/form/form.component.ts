80a1e9a428cbb39a436efe6eb3ea7e39b805f764
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-05-20 22:53:56 +0700

    edit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,52 +10,50 @@ export class FormComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-  user = [
+  users = [
     {
-      id:1,
+      id: 1,
       name: 'duynvph13636',
       age: 20,
       phone: 84,
-      email: '',
+      email: 'duynvph@gmail.com',
     },
   ];
   inputValue = {
-    id:0,
+    id: 0,
     name: '',
     age: 0,
-    phone:0,
-    email:''
+    phone: 0,
+    email: '',
   };
-  onSubmit(formData: {
-    name: string;
-    age: number;
-    phone: number;
-    email: string;
-  }) {
-    // console.log(formData);
-    const newUserIds = this.user.map(user=>user.id).sort((a:number,b:number)=>b-a);
-    const maxId=newUserIds[0];
-    this.user.push({...formData,id:maxId+1});
-    this.inputValue = {
-      id:0,
-      name: '',
-      age: 0,
-      phone:0,
-      email:''
-    };
-    
-  }
-  buttonDelete(id:number){
-    this.user=this.user.filter((item)=>item.id!==id)
-  }
-  buttonEdit(id:number){
-    const updateId = this.user.find((user:any)=>user.id===id);
-    this.inputValue={
-      id:updateId.id,
-      name:updateId.name,
-      age:updateId.age,
-      phone:updateId.phone,
-      email:updateId.email
+  onSubmit(userForm: NgForm) {
+    console.log(userForm.value);
+    if (this.inputValue.id) {
+      this.users = this.users.map((user: any) =>
+        user.id === this.inputValue.id ? this.inputValue : user
+      );
+      userForm.resetForm({ name: '', age: 0, email: '', phone: 0 });
+    } else {
+      const newUserIds = this.users
+        .map((user) => user.id)
+        .sort((a: number, b: number) => b - a);
+      const maxId = newUserIds[0];
+      this.users.push({ ...userForm.value, id: maxId + 1 });
+      userForm.resetForm({ name: '', age: 0, email: '', phone: 0 });
     }
+  }
+  buttonDelete(id: number) {
+    this.users = this.users.filter((item) => item.id !== id);
+  }
+  buttonEdit(id: number) {
+    const updateId = this.users.find((user: any) => user.id === id);
+    this.inputValue={
+      id:updateId?.id,
+      name:updateId?.name,
+      age:updateId?.age,
+      phone:updateId?.phone,
+      email:updateId?.email
+    }
+   
   }
 }
