91ded80e45c29374eb10ffb90e39fe19b06ba1cb
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-05-20 09:15:38 +0700

    bh5

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,16 +3,58 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  user = [
+    {
+      id:1,
+      name: 'duynvph13636',
+      age: 20,
+      phone: 84,
+      email: '',
+    },
+  ];
+  inputValue = {
+    id:0,
+    name: '',
+    age: 0,
+    phone:0,
+    email:''
+  };
+  onSubmit(formData: {
+    name: string;
+    age: number;
+    phone: number;
+    email: string;
+  }) {
+    // console.log(formData);
+    const newUserIds = this.user.map(user=>user.id).sort((a:number,b:number)=>b-a);
+    const maxId=newUserIds[0];
+    this.user.push({...formData,id:maxId+1});
+    this.inputValue = {
+      id:0,
+      name: '',
+      age: 0,
+      phone:0,
+      email:''
+    };
+    
   }
-onSubmit(formData:{name:string,age:string}){
-  console.log(formData);
-  
-}
+  buttonDelete(id:number){
+    this.user=this.user.filter((item)=>item.id!==id)
+  }
+  buttonEdit(id:number){
+    const updateId = this.user.find((users:any)=>users.id==id);
+    this.inputValue={
+      id:updateId.id,
+      name:updateId.name,
+      age:updateId.age,
+      phone:updateId.phone,
+      email:updateId.email
+    }
+  }
 }
